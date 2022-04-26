1b0428095a8d18ff4cfdc1a9ba31755443cb92a3
E Ferreira <98027423+emffor@users.noreply.github.com>
2022-04-26 10:24:16 -0300

    fix: firestore realtime database
    - Atualização em tempo real
    
    - Implementar o real time do Firestore que observa toda a coleção para ver quando algum documento é alterado (removido, adicionado, modificado) e refletir em tempo real na aplicação.

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -10,23 +10,22 @@ import { Product, ProductProps } from '../Product';
 export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
 
-  //usar o useEffect ler os documentos de uma coleção e carregar os documentos no "products" para ser listado na <FlatList/>.
   useEffect(() => {
-    firestore()
+    const subscribe = firestore()
     .collection('products')
-    .get()
-    .then(response => {
-      const data = response.docs.map(doc => {
+    .onSnapshot(querySnapshot => {
+      const data = querySnapshot.docs.map(doc => {
         return {
           id: doc.id,
           ...doc.data()
-        }
+        };
       }) as ProductProps[];
-      
+
       setProducts(data);
-    })
-    .catch(error => console.error(error));
-  }, []); 
+    });
+
+    return () => subscribe();
+  }, []);
   
 /*   //LEITURA DE UM ÚNICO DOCUMENTO
   useEffect(() => {
